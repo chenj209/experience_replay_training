f52f70aa68c3727409352b8e10145b26631eea63
chenj209 <kinderjianda.chen@gmail.com>
2023-11-27 23:29:29 -0500

    offline test

--- models/load_models.py
+++ models/load_models.py
@@ -2,6 +2,7 @@ import os
 import time
 import sys
 import numpy as np
+import re
 #from tqdm.notebook import tqdm
 
 #from dataloader_subset_files import Dataset
@@ -95,6 +96,12 @@ def load_models(model029, model3059, model6164, model6165, visible_gpus=[0,1,2,3
                 model = models.ResMLP(130,30,node_size, activation, num_blocks)
             elif model_type == "pos2":
                 model = models.ResMLP(124,30,node_size, activation, num_blocks)
+            elif model_type.startswith("resmlp"): 
+                m = re.match(r"resmlp_(\d+)_(\d+)", model_type)
+                input_size = m.group(1)
+                output_size = m.group(2)
+                print(f"Loading  Resmlp {input_size}:{output_size}")
+                model = models.ResMLP(input_size,output_size,node_size, activation, num_blocks)
             else:
                 print("Loading regular resnet30")
                 model = models.ResNet_output30(node_size, activation, num_blocks)
