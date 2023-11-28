0292f8ea44df8e472cadfe549022c349081090fb
chenj209 <kinderjianda.chen@gmail.com>
2023-11-27 23:48:32 -0500

    train nn pacific time

--- training_scripts/scripts029_pacific_time.py
+++ training_scripts/scripts029_pacific_time.py
@@ -1,7 +1,7 @@
 import os
 import sys
 for epoch in ['50']:
-        for lr in ['0.0001']:
+        for lr in ['0.001']:
             for batch_size in ['32']:
                 for noise_std in ['0']:
                     dropout = '0'
@@ -11,7 +11,7 @@ for epoch in ['50']:
 
                     network = sys.argv[1]
                     print("Training network: ", network)
-                    name = 'modelpacific_0-29_{}_act{}_bs{}_scheduler_{}_lr{}_ep{}_noise{}_wd{}_dropout{}_wxnorm'.format(
+                    name = 'timemodelpacific_0-29_{}_act{}_bs{}_scheduler_{}_lr{}_ep{}_noise{}_wd{}_dropout{}_wxnorm'.format(
                         network, activation, batch_size, lr_strategy, lr, epoch, noise_std, weight_decay, dropout)
                     print(name)
                     commands = 'CUDA_VISIBLE_DEVICES={} python train_nn_region_time.py --region_mask pacific_region_mask.npy --data_dir /home/users/data/nncam_data/image_set/ --output_type 0-29 --noise_std {} ' \
