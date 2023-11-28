19f3bd162365d5ba01e6243a6903dc46383c952c
chenj209 <kinderjianda.chen@gmail.com>
2023-11-27 23:34:41 -0500

    load models adding regex matching

--- models/load_models.py
+++ models/load_models.py
@@ -98,8 +98,8 @@ def load_models(model029, model3059, model6164, model6165, visible_gpus=[0,1,2,3
                 model = models.ResMLP(124,30,node_size, activation, num_blocks)
             elif model_type.startswith("resmlp"): 
                 m = re.match(r"resmlp_(\d+)_(\d+)", model_type)
-                input_size = m.group(1)
-                output_size = m.group(2)
+                input_size = int(m.group(1))
+                output_size = int(m.group(2))
                 print(f"Loading  Resmlp {input_size}:{output_size}")
                 model = models.ResMLP(input_size,output_size,node_size, activation, num_blocks)
             else:
