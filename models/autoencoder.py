1c121c248a00e00f662ecdabed97312243eb60a4
chenj209 <kinderjianda.chen@gmail.com>
2024-01-13 19:42:58 -0500

    new autoencoder

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,172 +1,153 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import sys
 
-import torch
-import torch.nn as nn
-
+sys.path.append('../utils')
+from data_shape import to_inference_shape_torch
 from models import ResMLP
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, latent_size):
+    def __init__(self, input_dim):
         super(Encoder, self).__init__()
-        self.input_size = input_size
-        self.latent_size = latent_size
-        self.e0 = nn.Conv2d(input_size, 128, kernel_size=1, padding=0, stride=1)
-        # Convolutional layers
-        self.e11 = nn.Conv2d(128, 256, kernel_size=3, padding=1, stride=1) # output: 122x96x184
-        self.e12 = nn.Conv2d(256, 256, kernel_size=3, padding=1, stride=1) # output: 256x96x184
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 256x48x92
-
-        # input: 256x48x92
-        self.e21 = nn.Conv2d(256, 512, kernel_size=3, padding=1, stride=1) # output: 512x48x92
-        self.e22 = nn.Conv2d(512, 512, kernel_size=3, padding=1, stride=1) # output: 512x48x92
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 512x24x46
-
-        # input: 512x24x46
-        self.e31 = nn.Conv2d(512, 1024, kernel_size=3, padding=1, stride=1) # output: 1024x24x46
-        self.e32 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1, stride=1) # output: 1024x24x46
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 1024x12x23
-
-        # input: 1024x12x23
-        self.e41 = nn.Conv2d(1024, 2048, kernel_size=3, padding=1, stride=1) # output: 2048x12x23
-        self.e42 = nn.Conv2d(2048, 2048, kernel_size=3, padding=1, stride=1) # output: 2048x12x23
-
-        # input: 2048x12x23
-        self.fc = nn.Linear(2048*12*18, latent_size*96*144)
+        self.conv1 = nn.Conv2d(input_dim, 512, kernel_size=3, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(512)  # Batch normalization for the first layer
+        self.conv2 = nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(1024)  # Batch normalization for the second layer
+        self.conv3 = nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(2048)  # Batch normalization for the third layer
+        # compute the flattened size
+        self.flattened_size = 2048 * 12 * 18
+        self.fc = nn.Linear(self.flattened_size, 256)
 
     def forward(self, x):
-        e0 = self.e0(x)
-        # Encoder
-        e11 = self.e11(e0)
-        e11 = nn.ReLU(inplace=True)(e11)
-        e12 = self.e12(e11)
-        e12 = nn.ReLU(inplace=True)(e12)
-        pool1 = self.pool1(e12)
-
-        e21 = self.e21(pool1)
-        e21 = nn.ReLU(inplace=True)(e21)
-        e22 = self.e22(e21)
-        e22 = nn.ReLU(inplace=True)(e22)
-        pool2 = self.pool2(e22)
-
-        e31 = self.e31(pool2)
-        e31 = nn.ReLU(inplace=True)(e31)
-        e32 = self.e32(e31)
-        e32 = nn.ReLU(inplace=True)(e32)
-        pool3 = self.pool3(e32)
-
-        e41 = self.e41(pool3)
-        e41 = nn.ReLU(inplace=True)(e41)
-        e42 = self.e42(e41)
-        e42 = nn.ReLU(inplace=True)(e42)
-        e42 = torch.flatten(e42, 1)
-        latent = self.fc(e42)
-        return latent
-
-class Decoder(nn.Module):
-    def __init__(self, input_size, latent_size):
-        super(Decoder, self).__init__()
-        self.input_size = input_size
-        self.latent_size = latent_size
-        # Fully connected layer
-        self.fc = nn.Linear(latent_size*96*144, 2048 * 12 * 18)
-        # Decoder
-        # input 2048x12x23
-        self.upconv1 = nn.ConvTranspose2d(2048, 1024, kernel_size=2, stride=2) # output: 1024x24x46
-        self.d11 = nn.Conv2d(2048, 1024, kernel_size=3, padding=1) # output: 1024x24x46
-        self.d12 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 1024x24x46
-
-        self.upconv2 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2) # output: 512x48x92
-        self.d21 = nn.Conv2d(1024, 512, kernel_size=3, padding=1) # output: 512x48x92
-        self.d22 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 512x48x92
-
-        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2) # output: 256x96x184
-        self.d31 = nn.Conv2d(512, 256, kernel_size=3, padding=1) # output: 256x96x184
-        self.d32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 256x96x184
-
-        # Output layer
-        self.outconv = nn.Conv2d(256, input_size, kernel_size=1)
-
-    def forward(self, x):
-        # Map from latent space to spatial dimensions
-        x = self.fc(x)
-        x = x.view(-1, 2048, 12, 18)  # Reshape to match the output of the encoder's last convolutional layer
-
-        # Apply deconvolutional layers
-        x = self.relu(self.bn1(self.conv_transpose1(x)))
-        x = self.relu(self.bn2(self.conv_transpose2(x)))
-        x = self.relu(self.bn3(self.conv_transpose3(x)))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = x.view(-1, self.flattened_size)
+        x = F.relu(self.fc(x))
         return x
 
-# class Autoencoder(nn.Module):
-#     def __init__(self):
-#         super(Autoencoder, self).__init__()
-#         self.encoder = Encoder()
-#         self.decoder = Decoder()
+# write a correspoinding decoder
+class Decoder(nn.Module):
+    def __init__(self, output_dim):
+        super(Decoder, self).__init__()
+        self.fc = nn.Linear(256, 2048 * 12 * 18)
 
-#     def forward(self, x):
-#         x = self.encoder(x)
-#         x = self.decoder(x)
-#         return x
-
-
-class AutoencoderResMLP(nn.Module):
-    def __init__(self, input_size, output_size, m, activation, num_blocks, latent_size):
-        super(AutoencoderResMLP, self).__init__()
-        self.input_size = input_size
-        self.latent_size = latent_size
-        self.encoder = Encoder(input_size, latent_size)
-        self.decoder = Decoder(input_size, latent_size)
-        self.resmlp = ResMLP(input_size, output_size, m, activation, num_blocks)
+        self.deconv1 = nn.ConvTranspose2d(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn1 = nn.BatchNorm2d(1024)  # Batch normalization for the first layer
+        self.deconv2 = nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn2 = nn.BatchNorm2d(512)  # Batch normalization for the second layer
+        self.deconv3 = nn.ConvTranspose2d(512, output_dim, kernel_size=3, stride=2, padding=1, output_padding=1)
 
     def forward(self, x):
-        curr_x = x[:, -122:]
+        x = F.relu(self.fc(x))
+        x = x.view(-1, 2048, 12, 18)
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = torch.sigmoid(self.deconv3(x))  # Using sigmoid for the final layer
+        return x
+
+class Encoder3D(nn.Module):
+    def __init__(self):
+        super(Encoder3D, self).__init__()
+        self.conv1 = nn.Conv3d(5, 16, kernel_size=3, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm3d(16)  # Batch normalization for the first layer
+        self.conv2 = nn.Conv3d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm3d(32)  # Batch normalization for the second layer
+        self.conv3 = nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm3d(64)  # Batch normalization for the third layer
+
+        self.flattened_size = 64 * 4 * 12 * 18
+        self.fc = nn.Linear(self.flattened_size, 256)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = x.view(-1, self.flattened_size)
+        x = F.relu(self.fc(x))
+        return x
+
+class Decoder3D(nn.Module):
+    def __init__(self):
+        super(Decoder3D, self).__init__()
+        self.fc = nn.Linear(256, 64 * 4 * 12 * 18)
+
+        self.deconv1 = nn.ConvTranspose3d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn1 = nn.BatchNorm3d(32)  # Batch normalization for the first layer
+        self.deconv2 = nn.ConvTranspose3d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn2 = nn.BatchNorm3d(16)  # Batch normalization for the second layer
+        self.deconv3 = nn.ConvTranspose3d(16, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.fc(x))
+        x = x.view(-1, 64, 4, 12, 18)
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = torch.sigmoid(self.deconv3(x))  # Using sigmoid for the final layer
+        return x
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_size):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder(input_size)
+        self.decoder = Decoder(input_size)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class AutoencoderResMLP(nn.Module):
+    def __init__(self, input_size, output_size, m, activation, num_blocks, latent_dim=4):
+        super(AutoencoderResMLP, self).__init__()
+        self.encoder = Encoder(input_size)
+        self.decoder = Decoder(input_size)
+        self.resmlp = ResMLP(122+latent_dim, output_size, m, activation, num_blocks)
+        self.fc = nn.Linear(256, 4*96*144)
+
+    def forward(self, x):
         latent = self.encoder(x)
-        x_rec = self.decoder(latent)
-        latent = latent.view(-1, self.latent_size, 96,144)
-        x = torch.cat((curr_x, latent), dim=1) # x_concat (batch, 122+latent_size, 96, 144)
-        # transpose and make x_concat (batch*96*144, channels)
-        x = x.transpose(1,2).transpose(2,3).reshape(-1, 122+self.latent_size)
-        x = self.resmlp(x)
-        return x, x_rec
+        x_resmlp = x[:, :122, :, :]
+        x_resmlp_ex = F.relu(self.fc(latent))
+        x_resmlp_ex = x_resmlp_ex.view(-1, 4, 96, 144)
+        x_resmlp = torch.cat((x_resmlp, x_resmlp_ex), dim=1)
+        # print(x_resmlp.shape)
+        x_resmlp = to_inference_shape_torch(x_resmlp)
+        x_resmlp = self.resmlp(x_resmlp)
+        x = self.decoder(latent)
+        return x_resmlp, x
 
-
-if __name__ == "__main__":
-    from torch.profiler import profile, record_function, ProfilerActivity
-    batch_size = 1
+if __name__ == '__main__':
     input_size = 340
-    model = AutoencoderResMLP(input_size,30,512,"relu", 7,4)
-    print("Model loaed")
-    # Assuming you're using a GPU
-    model.float().cuda()
-    input_data = torch.randn(batch_size, input_size, 96, 144).float().cuda()
-    target_data = torch.randn(96*144, 30).float().cuda()
-    criterion = nn.MSELoss()
+    autoencoder = Autoencoder(input_size)
+    print(autoencoder)
 
-    # Memory for model parameters
-    param_memory = sum(p.numel() * p.element_size() for p in model.parameters())
-    print(f"Parameter memory: {param_memory / (1024**2):.2f} MB")
+    # Example input
+    input_data = torch.randn(1, 340, 96, 144)  # Batch size of 1
+    output = autoencoder(input_data)
+    print(output.shape)  # Should be the same as input_data's shape
 
-    # Forward pass
-    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
-        with record_function("model_inference"):
-            #model(input_data)
-            output1, output2 = model(input_data)
-    print(prof.key_averages().table(sort_by="cuda_memory_usage", row_limit=10))
-    print(output1.shape)
-    print(output2.shape)
-    torch.cuda.synchronize()  # Wait for operations to finish
-    forward_memory = torch.cuda.memory_allocated()
-    print(f"Forward pass memory: {forward_memory / (1024**2):.2f} MB")
+    for name, module in autoencoder.named_modules():
+        num_params = sum(p.numel() for p in module.parameters(recurse=False))
+        if num_params > 0:
+            layer_size_gb = (num_params * 4) / (1024**3)  # Calculating size in GB
+            print(f"{name}: {type(module).__name__}, Parameters: {num_params}, Size: {layer_size_gb:.6f} GB")
 
-    # Backward pass
-    loss1 = criterion(output1, target_data)
-    loss2 = criterion(output2, input_data)
-    loss = loss1+loss2
-    loss.backward()
-    torch.cuda.synchronize()
-    peak_memory = torch.cuda.max_memory_allocated()
 
-    # Print memory usage
-    print(f"Peak memory during forward and backward pass: {peak_memory / (1024**2):.2f} MB")
+    autoencoder = AutoencoderResMLP(input_size, 30, 512, 'relu', 7)
+    print(autoencoder)
+
+    # Example input
+    input_data = torch.randn(1, 340, 96, 144)  # Batch size of 1
+    output1, output2 = autoencoder(input_data)
+    print(output1.shape)  # Should be the same as input_data's shape
+    print(output2.shape)  # Should be the same as input_data's shape
+
+    for name, module in autoencoder.named_modules():
+        num_params = sum(p.numel() for p in module.parameters(recurse=False))
+        if num_params > 0:
+            layer_size_gb = (num_params * 4) / (1024**3)  # Calculating size in GB
+            print(f"{name}: {type(module).__name__}, Parameters: {num_params}, Size: {layer_size_gb:.6f} GB")
--- utils/data_shape.py
+++ utils/data_shape.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def to_inference_shape(data):
     """
@@ -7,6 +8,16 @@ def to_inference_shape(data):
     """
     data = np.transpose(data, (0, 2, 3, 1))
     data = np.reshape(data, (-1, data.shape[-1]))
+    return data
+
+# write a torch version of to_inference_shape
+def to_inference_shape_torch(data):
+    """
+    the raw data is in format of (batch, channels, lat, lon)
+    the output data is in shape of (batch*lat*lon, channels)
+    """
+    data = data.permute(0, 2, 3, 1)
+    data = data.reshape(-1, data.shape[-1])
     return data
 
 def inverse_to_inference_shape(data):
