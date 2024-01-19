c926e61554f9dbde8ef2d9394dbef02a0d8c9a7e
chenj209 <kinderjianda.chen@gmail.com>
2024-01-18 23:14:05 -0500

    adding region mask to train tiem model ex

--- training_scripts/train_time_model_ex.py
+++ training_scripts/train_time_model_ex.py
@@ -19,6 +19,7 @@ from utils import Logger, AverageMeter, mkdir_p
 sys.path.append(os.path.join(sys.path[0], "..", "utils"))
 import argsparser
 import tools
+from data_shape import to_inference_shape, inverse_to_inference_shape
 sys.path.append(os.path.join(sys.path[0], "..", "dataloader"))
 from dataloader_newformat import DatasetDisk
 
@@ -175,6 +176,13 @@ def main(args):
     # def my_collate(batch):
     #     batch = list(filter (lambda x:x is not None, batch))
     #     return default_collate(batch)
+    region_mask = None
+    if args.region_mask is not None or args.region_mask == "all":
+        region_mask = np.load(args.region_mask)[None, None, :, :]
+        region_mask = to_inference_shape(region_mask).squeeze()
+    else:
+        region_mask = np.ones((1, 1, 96, 144))
+        region_mask = to_inference_shape(region_mask).squeeze()
 
     # Train and test
     current_iters = 0
@@ -192,13 +200,13 @@ def main(args):
             batch[1] = batch[1].reshape(-1, batch[1].shape[-1])
             lr = lr_scheduler[args.lr_strategy](optimizer, args.lr, current_iters, len(trainloader) * args.epoch)
             if args.output_type == '0-29':
-                batch[1] = batch[1][:, :30]
+                batch[1] = batch[1][:, region_mask.astype(bool), :30]
             if args.output_type == '30-59':
-                batch[1] = batch[1][:, 30:60]
+                batch[1] = batch[1][:, region_mask.astype(bool), 30:60]
             if args.output_type == '60':
-                batch[1] = batch[1][:, 60:61]
+                batch[1] = batch[1][:, region_mask.astype(bool), 60:61]
             if args.output_type == '61-65':
-                batch[1] = batch[1][:, 61:66]
+                batch[1] = batch[1][:, region_mask.astype(bool), 61:66]
 #             if args.output_type == '61-65':
 #                 train_mse = tools.train_penalty(batch, model, criterion, optimizer)
 #             else:
@@ -226,13 +234,13 @@ def main(args):
             batch[1] = batch[1].reshape(-1, batch[1].shape[-1])
             suffix = 'testing- epoch:{}| iters:{}/{} |'.format(epoch, iter+1, len(testloader))
             if args.output_type == '0-29':
-                batch[1] = batch[1][:, :30]
+                batch[1] = batch[1][:, region_mask.astype(bool), :30]
             if args.output_type == '30-59':
-                batch[1] = batch[1][:, 30:60]
+                batch[1] = batch[1][:, region_mask.astype(bool), 30:60]
             if args.output_type == '60':
-                batch[1] = batch[1][:, 60:61]
+                batch[1] = batch[1][:, region_mask.astype(bool), 60:61]
             if args.output_type == '61-65':
-                batch[1] = batch[1][:, 61:66]
+                batch[1] = batch[1][:, region_mask.astype(bool), 61:66]
             test_mses = tools.test_de(batch, model, criterion)
             for i in range(1):
                 test_mse = test_mses[i]
@@ -270,6 +278,7 @@ if __name__ == '__main__':
     parser.add_argument("--multistep", type=int, help="multistep", default=1)
     parser.add_argument("--sample_rate", type=int, help="sample_rate", default=12)
     parser.add_argument("--ex_input", type=str, nargs="*")
+    parser.add_argument("--region_mask", type=str, help="path to region mask npy file", default="all")
     args = parser.parse_args()
     print(args)
 
