bff8c915fbefb62b71e467e879b3fc7266e0d860
jiandachen <kindercjd@gmail.com>
2024-01-23 08:54:49 -0800

    bugged

--- offline_test/offline_test_newformat.py
+++ offline_test/offline_test_newformat.py
@@ -69,8 +69,10 @@ def offline_test(args, all_models, testloader, get_thickness, inverse_output, si
         #model.eval()
         with torch.no_grad():
             points_x, points_y, x_raw, _ = batch
+            print("x_raw:", x_raw.shape)
             if get_thickness is not None:
-                thickness = get_thickness(x_raw[0,region_mask,121].numpy())
+                thickness = get_thickness(x_raw[0,:,-1].numpy())
+                print("thickness:", thickness.shape)
             #points_x, points_y = (points_x.float()).cuda(), (points_y.float()).cuda()
             points_x = (points_x.float()).cuda()
             y1 = inverse_output['qtend_check'](all_models['0_29'](points_x).detach()
