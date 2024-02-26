2b0e53d6139dfeed39d900f0d0c92a6496343e14
jiandachen <kindercjd@gmail.com>
2024-02-25 17:53:19 -0800

    fix training slurm

--- training_scripts/slurm_train_time.sh
+++ training_scripts/slurm_train_time.sh
@@ -2,7 +2,7 @@
 ##SBATCH -A m4402
 #SBATCH -A m4359
 #SBATCH -C gpu
-#SBATCH --qos=regular
+#SBATCH --qos=shared
 #SBATCH -t 8:00:00
 #SBATCH -n 1
 #SBATCH -c 32
@@ -20,5 +20,5 @@ conda activate mpi4py2
 #conda activate pytorch1
 python -c "import torch; print(torch.zeros(1).cuda())"
 python -c "import torch; print(torch.cuda.is_available())"
-echo "region" $1 "ex_input" $2
-srun --constraint=gpu --ntasks 1 -G 1 python scripts_train_time_model029_ex.py  --region_mask $1 --ex_input $2
+echo "ex_input_prev" $1 "ex_input" $2
+srun --constraint=gpu --ntasks 1 -G 1 python scripts_train_time_model029_ex.py  --region_mask ../consts/pacific_region_mask.npy --ex_input $2 --ex_input_prev $1
