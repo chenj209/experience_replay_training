fix kl loss and change log

training_scripts/train_ae_model_ex.py
@@ -258,12 +258,12 @@ def main(args):
                 loss_pred = criterion(outputs_y, points_y)
             l1_penalty = sum(torch.abs(param).sum() for param in model.parameters())
             kl_divergence = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-            loss_rec = criterion(x_rec, points_x)+kl_divergence
+            loss_rec = criterion(x_rec, points_x)
             # loss = args.pred_weight*loss_pred + args.rec_weight*(loss_rec)
-            if iter < 2:
-                loss = loss_rec + ae_config["l1"]*l1_penalty
+            if epoch < 1:
+                loss = loss_rec + kl_divergence + ae_config["l1"]*l1_penalty
             else:
-                loss = args.pred_weight*loss_pred + args.rec_weight*loss_rec + ae_config["l1"]*l1_penalty
+                loss = args.pred_weight*loss_pred + args.rec_weight*(loss_rec+kl_divergence) + ae_config["l1"]*l1_penalty
             # print(points_y)
             # print(torch.min(points_y))
             # compute gradient and do SGD step
@@ -274,10 +274,10 @@ def main(args):
             train_mse = loss.item()
             train_losses.update(train_mse, batch[0].size(0))
             current_iters += 1
-            print('training- epoch:{}/{} | iters:{}/{}| lr:{:.6f} | \
-                  train pred mse:{:.6f}| train rec mse: {:.6f} | l1: {:.2e}'.format(
+            print('training- epoch:{}/{} | iters:{}/{}| lr:{:.2e} | \
+                  train pred mse:{:.2e}| train rec mse: {:.2e} | kl: {:.2e} | l1: {:.2e}'.format(
                       epoch, args.epoch, iter+1, len(trainloader),
-                      lr, loss_pred.item(), loss_rec.item(), l1_penalty.item()))
+                      lr, loss_pred.item(), loss_rec.item(), kl_divergence.item(), l1_penalty.item()))
             # print('training- epoch:{}/{} | iters:{}/{}| lr:{:.6f} | \
             #       train pred mse:{:.6f}| train rec mse: {:.6f}'.format(
             #           epoch, args.epoch, iter+1, len(trainloader),
@@ -290,10 +290,11 @@ def main(args):
         #### define the loss seperately ## we have 7 mse accordingly
 
         test_losses = {}
-        for i in range(2):
+        for i in range(3):
             test_losses[i] = AverageMeter()
-        loss_name = [args.output_type + '_pred: {:.5f}']
-        loss_name.append(args.output_type + '_rec: {:.5f}')
+        loss_name = [args.output_type + '_pred: {:.2e} | ']
+        loss_name.append(args.output_type + '_rec: {:.2e} | ')
+        loss_name.append(args.output_type + '_kl: {:.2e}')
         test_time_begin = time.time()
         for iter, batch in enumerate(testloader):
             if batch is None:
@@ -328,9 +329,10 @@ def main(args):
             if outputs_y is not None:
                 loss_pred = criterion(outputs_y, points_y).item()
             kl_divergence = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-            loss_rec = criterion(x_rec, points_x).item() + kl_divergence.item()
+            loss_rec = criterion(x_rec, points_x).item()
             test_losses[0].update(loss_pred, batch[0].size(0))
             test_losses[1].update(loss_rec, batch[0].size(0))
+            test_losses[2].update(kl_divergence.item(), batch[0].size(0))
                 # suffix = suffix + loss_name[i].format(1 - test_losses[i].avg/test_variance[args.output_type])
             suffix = suffix + loss_name[0].format(test_losses[0].avg)
             suffix = suffix + loss_name[1].format(test_losses[1].avg)