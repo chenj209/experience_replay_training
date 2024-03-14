4542ac2183271345a28660cc817dc7b8c2f30a65
chenj209 <kinderjianda.chen@gmail.com>
2024-03-14 00:04:04 -0400

    by level norm

---
+++ dataloader/get_std_mean_newformat_by_level.py
@@ -0,0 +1,190 @@
+import concurrent.futures
+import numpy as np
+import glob
+import argparse
+import re
+from dataloader_newformat import get_index_from_colnames
+from tqdm.autonotebook import tqdm
+def process_file(fn, var_names, col_names, region_mask, debug):
+    data = np.load(fn)
+    data_sums = {var_name: 0 for var_name in var_names}
+    all_data = {var_name: [] for var_name in var_names} if debug else None
+
+    for var_name in var_names:
+        start, end = get_index_from_colnames(col_names, var_name)
+        idx = list(range(start, end))
+        cur_data = data[idx][:, region_mask].astype(np.float64)
+        data_sums[var_name] += cur_data.sum()
+
+        if debug:
+            all_data[var_name].append(cur_data)
+
+    return data_sums, all_data
+
+def process_file_std(fn, var_names, col_names, region_mask, data_means):
+    data = np.load(fn)
+    data_std_sums = {var_name: 0 for var_name in var_names}
+
+    for var_name in var_names:
+        start, end = get_index_from_colnames(col_names, var_name)
+        idx = list(range(start, end))
+        cur_data = data[idx][:, region_mask].astype(np.float64)
+        data_std_sums[var_name] += ((cur_data - data_means[var_name])**2).sum()
+
+    return data_std_sums
+
+def region_slice2d(region_mask2d):
+    mask, pad = region_mask2d
+    min_x = np.min(np.where(mask)[0])
+    max_x = np.max(np.where(mask)[0])
+    min_y = np.min(np.where(mask)[1])
+    max_y = np.max(np.where(mask)[1])
+    return min_x-pad, max_x+pad, min_y-pad, max_y+pad 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datapath", type=str, default="./data/")
+    parser.add_argument("--col_names", type=str, default="./data/col_names.txt")
+    parser.add_argument("--region_mask", type=str, default="all")
+    parser.add_argument("--out_name", type=str, default="test_data")
+    parser.add_argument("--debug", action="store_true")
+
+    args = parser.parse_args()
+    if args.region_mask == "all":
+        region_mask = np.ones((96,144)).astype(bool)
+    else:
+        region_mask = np.load(args.region_mask)
+        min_x, max_x, min_y, max_y = region_slice2d((region_mask, 2))
+        region_mask = np.zeros((96,144))
+        region_mask[min_x:max_x, min_y:max_y] = 1
+        region_mask = region_mask.astype(bool)
+
+    col_names = np.loadtxt(args.col_names, dtype=str)
+    # pattern = f"^(().*)(_lev\d+)?$"
+    pattern = f"^(.*?)(?=_lev\d+|$)"
+    var_names = col_names
+    # for col_name in col_names:
+    #     match = re.match(pattern, col_name)
+    #     if match and match.group(1) not in var_names:
+    #         var_names.append(match.group(1))
+    print(var_names)
+    all_files = glob.glob(args.datapath + "/*.npy")
+    all_files.sort()
+    all_files = all_files[:35040]
+    print(all_files[:10], "...", all_files[-10:])
+
+    # # remove bad files
+    # for i in range(17507,17531):
+    #     for file_name in all_files:
+    #         if str(i) in file_name:
+    #             all_files.remove(file_name)
+
+
+    # for i in ['00001', '00002', '00003', '08690', '08691', '17522', '17523','26210','26211']:
+    #     for file_name in all_files:
+    #         if i in file_name:
+    #             all_files.remove(file_name)
+    for i in range(17507,17530):
+            for file_name in all_files:
+                if str(i) in file_name:
+                    all_files.remove(file_name)
+
+        #print('after file num:', len(all_files))
+
+        #for i in ['00001', '08690', '17522', '26210', '09242']:
+    for i in ['00001', '08690', '17522', '26210']:
+        for file_name in all_files:
+            if i in file_name:
+                all_files.remove(file_name)
+    for i in ['35042', '43730', '52562']:
+        for file_name in all_files:
+            if i in file_name:
+                all_files.remove(file_name)
+    for i in range(55015,55056):
+        for file_name in all_files:
+            if str(i) in file_name:
+                all_files.remove(file_name)
+
+
+    sample_file = np.load(all_files[0])
+    # data_sums = {var_name: 0 for var_name in var_names}
+    # print(region_mask.shape)
+    # if args.debug:
+    #     all_data = {var_name: [] for var_name in var_names}
+    # for fn in tqdm(all_files):
+    #     data = np.load(fn)
+    #     # import ipdb; ipdb.set_trace()
+    #     for var_name in var_names:
+    #         start, end = get_index_from_colnames(col_names, var_name)
+    #         idx = list(range(start,end))
+    #         cur_data = data[idx][:,region_mask].astype(np.float64)
+    #         data_sums[var_name] += cur_data.sum()
+    #         if args.debug:
+    #             all_data[var_name].append(cur_data)
+    
+    data_sums = {var_name: 0 for var_name in var_names}
+    all_data = {var_name: [] for var_name in var_names} if args.debug else None
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_file, fn, var_names, col_names, region_mask, args.debug) for fn in all_files]
+        
+        # for future in concurrent.futures.as_completed(futures):
+        #     file_data_sums, file_all_data = future.result()
+        #     for var_name in var_names:
+        #         data_sums[var_name] += file_data_sums[var_name]
+        #         if args.debug:
+        #             all_data[var_name].extend(file_all_data[var_name])
+        with tqdm(total=len(all_files)) as progress:
+            for future in concurrent.futures.as_completed(futures):
+                file_data_sums, file_all_data = future.result()
+                for var_name in var_names:
+                    data_sums[var_name] += file_data_sums[var_name]
+                    if args.debug:
+                        all_data[var_name].extend(file_all_data[var_name])
+
+                # Update the progress bar
+                progress.update(1)
+    data_means = data_sums
+    for var_name in var_names:
+        start, end = get_index_from_colnames(col_names, var_name)
+        var_lvl = end-start
+        data_means[var_name] = data_sums[var_name] / (len(all_files)*np.sum(region_mask)*var_lvl)
+    # compute std
+    data_std_sums = {var_name: 0 for var_name in var_names}
+    # for fn in tqdm(all_files):
+    #     data = np.load(fn)
+    #     for var_name in var_names:
+    #         start, end = get_index_from_colnames(col_names, var_name)
+    #         idx = list(range(start,end))
+    #         cur_data = data[idx][:,region_mask].astype(np.float64)
+    #         data_std_sums[var_name] += ((cur_data - data_means[var_name])**2).sum()
+    # data_std_sums = {var_name: 0 for var_name in var_names}
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = {executor.submit(process_file_std, fn, var_names, col_names, region_mask, data_means): fn for fn in all_files}
+
+        with tqdm(total=len(all_files)) as progress:
+            for future in concurrent.futures.as_completed(futures):
+                file_data_std_sums = future.result()
+                for var_name in var_names:
+                    data_std_sums[var_name] += file_data_std_sums[var_name]
+
+                progress.update(1)
+
+
+    data_stds = data_std_sums
+    for var_name in var_names:
+        start, end = get_index_from_colnames(col_names, var_name)
+        var_lvl = end-start
+        data_stds[var_name] = np.sqrt(data_std_sums[var_name] / (len(all_files)*np.sum(region_mask)*var_lvl))
+
+    if args.debug:
+        # check std and mean by comparing it with the all_data_x and all_data_y
+        for var_name in var_names:
+            all_data[var_name] = np.concatenate(all_data[var_name], axis=1)
+            assert(np.allclose(all_data[var_name].mean(), data_means[var_name]))
+            assert(np.allclose(all_data[var_name].std(), data_stds[var_name]))
+
+    np.savez(args.out_name+"_means", **data_means)
+    np.savez(args.out_name+"_stds", **data_stds)
+
+
---
+++ dataloader/tests/test_dataloader_newformat_by_level.py
@@ -0,0 +1,291 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "utils"))
+import os
+import glob
+import argparse
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils import data
+
+from preprocess import FlattenSpatialTransform, StandardizeTransform
+from dataloader_utils import gen_multistep_col_indices, get_index_from_colnames
+from dataloader_newformat import DatasetDisk, filter_collate
+from debug_utils import print_mean_std_by_var
+# import torch transforms
+
+data_dir = "/home/users/data/nncam_data/image_set/"
+if not os.path.isdir(data_dir):
+    data_dir = "/data/nncam_data/image_set/"
+if not os.path.isdir(data_dir):
+    # data_dir = "./data/"
+    data_dir = "../analysis/test_data/"
+if not os.path.isdir(data_dir):
+    # data_dir = "./data/"
+    data_dir = "/pscratch/sd/c/chenjd21/spcam_new_data/"
+col_names_x = ["QL_lev4", "QL_lev18", "QL_lev29"]
+col_names_y = ["qtend_check_lev4", "qtend_check_lev18", "qtend_check_lev29"]
+# varaibles that are used as input in the previous time step
+prev_ex_vars = ["SOLL", "SOLLD", "SOLS", "SOLSD", "FSDS"]
+data_means = dict(np.load(data_dir + "/std_mean_by_level_means.npz"))
+data_stds = dict(np.load(data_dir + "/std_mean_by_level_stds.npz"))
+col_names = np.loadtxt(data_dir + "/col_names.txt", dtype=str)
+region_mask = np.load(os.path.join(os.path.dirname(__file__), "..", "..", \
+                                    "consts", "pacific_region_mask.npy"))
+assert region_mask.shape == (96,144)
+
+def test_single_column_multistep0():
+    file_names = glob.glob(data_dir + "*.npy")
+    file_names.sort()
+    # file_names = file_names[:10]
+    # file_names = [data_dir + fn for fn in file_names]
+    print("file_names:", file_names[:10])
+    col_names = np.loadtxt(data_dir + "/col_names.txt", dtype=str)
+
+    input_indices, prev_input_indices, output_indices = gen_multistep_col_indices(
+        col_names, prev_ex_vars, col_names_x, col_names_y, 1)
+    print("input_indices:", col_names[input_indices])
+    print("prev_input_indices:", col_names[prev_input_indices])
+    print("output_indices:", col_names[output_indices])
+
+    transform = transforms.Compose([
+        StandardizeTransform(
+            data_means,
+            data_stds,
+            col_names_x,
+            col_names_y,
+            col_names,
+            normalize_input=True,
+            normalize_output=True,
+            region_mask1d=region_mask
+            ),
+        FlattenSpatialTransform()
+        ])
+
+    training_set = DatasetDisk(
+        file_names,
+        input_indices,
+        prev_input_indices,
+        output_indices,
+        multistep=0,
+        sample_rate=12,
+        is_train=True,
+        transform=transform,
+        include_filename=True
+        )
+    trainloader = data.DataLoader(training_set, shuffle=False, batch_size=1, num_workers=1, collate_fn=filter_collate)
+    norm_data_x = []
+    norm_data_y = []
+    # start_idx, end_idx = get_index_from_colnames(col_names, "dqvls_nn_in")
+    for idx, batch in enumerate(trainloader):
+        if batch is None:
+            continue
+        x, y = batch[:2] # x: (batch, n_samples, n_features)
+        x = x.reshape(-1, x.shape[-1]) # x: (batch * n_samples, n_features)
+        y = y.reshape(-1, y.shape[-1])
+        filenames = batch[-1]
+        print(idx, x.size(), y.size(), filenames)
+        norm_data_x.append(x.numpy())
+        norm_data_y.append(x.numpy())
+    norm_data_x = np.concatenate(norm_data_x, axis=0)
+    norm_data_y = np.concatenate(norm_data_y, axis=0)
+    print("norm_data_x shape: ", norm_data_x.shape)
+    # cur_idx = 0
+    # for col in col_names_x:
+    #     start_idx, end_idx = get_index_from_colnames(col_names, col)
+    #     data_range = end_idx - start_idx
+    #     print(col, norm_data_x[:,:,cur_idx:cur_idx+data_range].mean(), \
+    #           norm_data_x[:,:,cur_idx:cur_idx+data_range].std())
+    #     cur_idx += data_range
+    print_mean_std_by_var(norm_data_x, col_names_x, col_names)
+    print("qtend_check: ", norm_data_y.mean(), norm_data_y.std())
+    
+
+def test_image_multistep0():
+    file_names = glob.glob(data_dir + "*.npy")
+    file_names.sort()
+    # file_names = file_names[:10]
+    # file_names = [data_dir + fn for fn in file_names]
+    print("file_names:", file_names[:10])
+
+    input_indices, prev_input_indices, output_indices = gen_multistep_col_indices(
+        col_names, prev_ex_vars, col_names_x, col_names_y, 1)
+    print("input_indices:", col_names[input_indices])
+    print("prev_input_indices:", col_names[prev_input_indices])
+    print("output_indices:", col_names[output_indices])
+
+    transform = transforms.Compose([
+        StandardizeTransform(
+            data_means,
+            data_stds,
+            col_names_x,
+            col_names_y,
+            col_names,
+            normalize_input=True,
+            normalize_output=True
+            ),
+        # FlattenSpatialTransform()
+        ])
+
+    training_set = DatasetDisk(
+        file_names,
+        input_indices,
+        prev_input_indices,
+        output_indices,
+        multistep=0,
+        sample_rate=12,
+        is_train=True,
+        transform=transform,
+        include_filename=True
+        )
+    trainloader = data.DataLoader(training_set, shuffle=False, batch_size=1, num_workers=1, collate_fn=filter_collate)
+    norm_data_x = []
+    norm_data_y = []
+    # start_idx, end_idx = get_index_from_colnames(col_names, "dqvls_nn_in")
+    for idx, batch in enumerate(trainloader):
+        if batch is None:
+            continue
+        x, y = batch[:2] # x: (batch, n_samples, n_features, lat, lon)
+        filenames = batch[-1]
+        print(idx, x.size(), y.size(), filenames)
+        norm_data_x.append(x.numpy())
+        norm_data_y.append(x.numpy())
+    norm_data_x = np.concatenate(norm_data_x, axis=0)
+    norm_data_y = np.concatenate(norm_data_y, axis=0)
+    # print("Q: ", norm_data_x[:,:30].mean(), norm_data_x[:,:30].std())
+    # print("T: ", norm_data_x[:,30:60].mean(), norm_data_x[:,30:60].std())
+    # print("dqvls: ", norm_data_x[:,60:90].mean(), norm_data_x[:,60:90].std())
+    # print("dTls: ", norm_data_x[:,90:120].mean(), norm_data_x[:,90:120].std())
+    # print("SOLIN: ", norm_data_x[:,120].mean(), norm_data_x[:,120].std())
+    # print("SPPS: ", norm_data_x[:,121].mean(), norm_data_x[:,121].std())
+    print_mean_std_by_var(norm_data_x, col_names_x, col_names)
+    print("qtend_check: ", norm_data_y.mean(), norm_data_y.std())
+
+def test_single_column_multistep1():
+    file_names = glob.glob(data_dir + "*.npy")
+    file_names.sort()
+    # file_names = file_names[:10]
+    # file_names = [data_dir + fn for fn in file_names]
+    print("file_names:", file_names[:10])
+
+    input_indices, prev_input_indices, output_indices = gen_multistep_col_indices(
+        col_names, prev_ex_vars, col_names_x, col_names_y, 1)
+    print("input_indices:", col_names[input_indices])
+    print("prev_input_indices:", col_names[prev_input_indices])
+    print("output_indices:", col_names[output_indices])
+
+    transform = transforms.Compose([
+        StandardizeTransform(
+            data_means,
+            data_stds,
+            col_names_x + prev_ex_vars + col_names_x,
+            col_names_y,
+            col_names,
+            normalize_input=True,
+            normalize_output=True
+            ),
+        FlattenSpatialTransform()
+        ])
+
+    training_set = DatasetDisk(
+        file_names,
+        input_indices,
+        prev_input_indices,
+        output_indices,
+        multistep=1,
+        sample_rate=1,
+        is_train=True,
+        transform=transform,
+        include_filename=True,
+        region_mask1d=region_mask
+        )
+    trainloader = data.DataLoader(training_set, shuffle=False, batch_size=1, num_workers=1, collate_fn=filter_collate)
+    norm_data_x = []
+    norm_data_y = []
+    # start_idx, end_idx = get_index_from_colnames(col_names, "dqvls_nn_in")
+    for idx, batch in enumerate(trainloader):
+        if batch is None:
+            continue
+        x, y = batch[:2] # x: (batch, n_samples, n_features)
+        x = x.reshape(-1, x.shape[-1]) # x: (batch * n_samples, n_features)
+        y = y.reshape(-1, y.shape[-1])
+        filenames = batch[-1]
+        print(idx, x.size(), y.size(), filenames)
+        norm_data_x.append(x.numpy())
+        norm_data_y.append(x.numpy())
+    norm_data_x = np.concatenate(norm_data_x, axis=0)
+    norm_data_y = np.concatenate(norm_data_y, axis=0)
+    print_mean_std_by_var(norm_data_x, col_names_x + prev_ex_vars + col_names_x, col_names)
+    print("qtend_check: ", norm_data_y.mean(), norm_data_y.std())
+
+def test_image_multistep1():
+    file_names = glob.glob(data_dir + "*.npy")
+    file_names.sort()
+    # file_names = file_names[:10]
+    # file_names = [data_dir + fn for fn in file_names]
+    print("file_names:", file_names[:10])
+    # col_names_x = col_names_x + prev_ex_vars + col_names_x
+
+    input_indices, prev_input_indices, output_indices = gen_multistep_col_indices(
+        col_names, prev_ex_vars, col_names_x, col_names_y, 1)
+    print("input_indices:", col_names[input_indices])
+    print("prev_input_indices:", col_names[prev_input_indices])
+    print("output_indices:", col_names[output_indices])
+
+    transform = transforms.Compose([
+        StandardizeTransform(
+            data_means,
+            data_stds,
+            col_names_x + prev_ex_vars + col_names_x,
+            col_names_y,
+            col_names,
+            normalize_input=True,
+            normalize_output=True,
+            include_raw=True
+            ),
+        # FlattenSpatialTransform()
+        ])
+
+    training_set = DatasetDisk(
+        file_names,
+        input_indices,
+        prev_input_indices,
+        output_indices,
+        multistep=1,
+        sample_rate=12,
+        is_train=True,
+        transform=transform,
+        include_filename=True,
+        region_mask2d=(region_mask,2)
+        )
+    trainloader = data.DataLoader(training_set, shuffle=False, batch_size=1, num_workers=1, collate_fn=filter_collate)
+    norm_data_x = []
+    norm_data_y = []
+    # start_idx, end_idx = get_index_from_colnames(col_names, "dqvls_nn_in")
+    for idx, batch in enumerate(trainloader):
+        if batch is None:
+            continue
+        x, y, x_raw, y_raw = batch[:4]
+        filenames = batch[-1]
+        print(idx, x.size(), y.size(), x_raw.size(), y_raw.size(), filenames)
+        norm_data_x.append(x.numpy())
+        norm_data_y.append(x.numpy())
+    norm_data_x = np.concatenate(norm_data_x, axis=0)
+    norm_data_y = np.concatenate(norm_data_y, axis=0)
+    # cur_idx = 0
+    # for col in col_names_x + prev_ex_vars + col_names_x:
+    #     start_idx, end_idx = get_index_from_colnames(col_names, col)
+    #     data_range = end_idx - start_idx
+    #     print(col, norm_data_x[:,cur_idx:cur_idx+data_range].mean(), \
+    #           norm_data_x[:,cur_idx:cur_idx+data_range].std())
+    #     cur_idx += data_range
+    print_mean_std_by_var(norm_data_x, col_names_x + prev_ex_vars + col_names_x, col_names)
+    print("qtend_check: ", norm_data_y.mean(), norm_data_y.std())
+
+if __name__ == "__main__":
+    # test_single_column_multistep0()
+    # test_image_multistep0()
+    test_single_column_multistep1()
+    test_image_multistep1()
+
