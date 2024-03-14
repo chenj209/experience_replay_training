b72812e8f8a66a807111325dae006cb03b583a3a
chenj209 <kinderjianda.chen@gmail.com>
2024-03-14 01:00:47 -0400

    adding debug utils

---
+++ dataloader/tests/debug_utils.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from dataloader_utils import get_index_from_colnames
+
+def print_mean_std_by_var(data, target_vars, col_names):
+    """
+    Print the mean and standard deviation of the data for each variable in target_vars.
+
+    data: shape (n_samples, n_features, ...)
+    target_vars: list of variable names
+    col_names: list of column names in order of appearance in the raw data
+    """
+    cur_idx = 0
+    for col in target_vars:
+        start_idx, end_idx = get_index_from_colnames(col_names, col)
+        data_range = end_idx - start_idx
+        print(col, data[:,cur_idx:cur_idx+data_range].mean(), \
+              data[:,cur_idx:cur_idx+data_range].std())
+        cur_idx += data_range
