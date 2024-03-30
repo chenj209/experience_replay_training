e3475742e1687369671a19fa483c49dca2e72a9c
chenj209 <kinderjianda.chen@gmail.com>
2024-03-30 18:38:39 -0400

    new norm not working

--- dataloader/tests/debug_utils.py
+++ dataloader/tests/debug_utils.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from dataloader_utils import get_index_from_colnames
 
-def print_mean_std_by_var(data, target_vars, col_names):
+def print_mean_std_by_var(data, target_vars, col_names, delevelwise=False):
     """
     Print the mean and standard deviation of the data for each variable in target_vars.
 
@@ -12,6 +12,13 @@ def print_mean_std_by_var(data, target_vars, col_names):
     target_vars: list of variable names
     col_names: list of column names in order of appearance in the raw data
     """
+    if delevelwise:
+        _target_vars = []
+        for col in target_vars:
+            delevelwise_var = col.split("_lev")[0]
+            if delevelwise_var not in _target_vars:
+                _target_vars.append(delevelwise_var)
+        target_vars = _target_vars
     cur_idx = 0
     for col in target_vars:
         start_idx, end_idx = get_index_from_colnames(col_names, col)
--- dataloader/tests/test_dataloader_newformat_new_inputs_all.py
+++ dataloader/tests/test_dataloader_newformat_new_inputs_all.py
@@ -10,10 +10,46 @@ import torchvision.transforms as transforms
 from torch.utils import data
 
 from preprocess import FlattenSpatialTransform, StandardizeTransform
-from dataloader_utils import gen_multistep_col_indices, get_index_from_colnames
+from dataloader_utils import gen_multistep_col_indices, levelwise_variable
 from dataloader_newformat import DatasetDisk, filter_collate
 from debug_utils import print_mean_std_by_var
+from logger import debug_print
 # import torch transforms
+
+DEBUG = True
+START_LEV = 6
+END_LEV = 29
+col_names_x = [
+    "QL_lev", 
+    "T_nn_in_lev", 
+    "dqvls_nn_in_lev", 
+    "dTls_nn_in_lev", 
+    "SOLIN", 
+    "SPPS",
+    "LWUP",
+    "CAPE",
+    "UL_lev",
+    "VL_lev"
+    ]
+col_names_x = levelwise_variable(col_names_x, START_LEV, END_LEV)
+prev_ex_vars = [
+    "qtend_check_lev", 
+    "stend_check_lev", 
+    "SOLL", 
+    "SOLLD", 
+    "SOLS", 
+    "SOLSD", 
+    "FSDS",
+    "CLOUD_lev",
+    "SPPRECC",
+    "FLNS",
+    "FLNT",
+    "SPQRL_lev",
+    "SPQRS_lev"
+    ]
+region_mask = np.load(os.path.join(os.path.dirname(__file__), "..", "..", \
+                                "consts", "pacific_region_mask.npy"))
+assert region_mask.shape == (96,144)
 
 data_dir = "/home/users/data/nncam_data/image_set/"
 if not os.path.isdir(data_dir):
@@ -25,6 +61,24 @@ if not os.path.isdir(data_dir):
     # data_dir = "./data/"
     data_dir = "/pscratch/sd/c/chenjd21/spcam_new_data/"
 
+data_means = dict(np.load(data_dir + "/data_means.npz"))
+data_means_by_lvl = dict(np.load(data_dir + "/std_mean_by_level_means.npz"))
+data_means.update(data_means_by_lvl)
+data_stds = dict(np.load(data_dir + "/data_stds.npz"))
+data_stds_by_lvl = dict(np.load(data_dir + "/std_mean_by_level_stds.npz"))
+data_stds.update(data_stds_by_lvl)
+
+# use the same mean and std for variables except for q related
+for k in data_means:
+    if "_lev" in k and \
+        ("QL" not in k and "qtend" not in k and "dqvls" not in k):
+        debug_print(f"Replacing means {k}({data_means[k]}) with\
+                     {k.rstrip('_lev')}({data_means[k.split('_lev')[0]]})", DEBUG)
+        data_means[k] = data_means[k.split("_lev")[0]]
+        debug_print(f"Replacing stds {k}({data_stds[k]}) with\
+                     {k.rstrip('_lev')}({data_stds[k.split('_lev')[0]]})", DEBUG)
+        data_stds[k] = data_stds[k.split("_lev")[0]]
+
 def test_single_column_multistep0():
     file_names = glob.glob(data_dir + "*.npy")
     file_names.sort()
@@ -32,14 +86,8 @@ def test_single_column_multistep0():
     # file_names = [data_dir + fn for fn in file_names]
     print("file_names:", file_names[:10])
     col_names = np.loadtxt(data_dir + "/col_names.txt", dtype=str)
-    col_names_x = ["QL", "T_nn_in", "dqvls_nn_in", "dTls_nn_in", "SOLIN", "SPPS", \
-                   "CLOUD", "CAPE", "LWUP", "SPPRECC", "FLNS", "FLNT", "SPQRL", "SPQRS"]
     col_names_y = ["qtend_check"]
     # varaibles that are used as input in the previous time step
-    prev_ex_vars = ["qtend_check", "stend_check", "SOLL", "SOLLD", "SOLS", "SOLSD", "FSDS"]
-
-    data_means = dict(np.load(data_dir + "/data_means.npz"))
-    data_stds = dict(np.load(data_dir + "/data_stds.npz"))
 
     input_indices, prev_input_indices, output_indices = gen_multistep_col_indices(
         col_names, prev_ex_vars, col_names_x, col_names_y, 1)
@@ -69,7 +117,8 @@ def test_single_column_multistep0():
         sample_rate=12,
         is_train=True,
         transform=transform,
-        include_filename=True
+        include_filename=True,
+        region_mask1d=region_mask
         )
     trainloader = data.DataLoader(training_set, shuffle=False, batch_size=1, 
                                   num_workers=1, collate_fn=filter_collate)
@@ -89,6 +138,7 @@ def test_single_column_multistep0():
     norm_data_y = np.concatenate(norm_data_y, axis=0)
     print("norm_data_x shape: ", norm_data_x.shape)
     print_mean_std_by_var(norm_data_x, col_names_x, col_names)
+    print_mean_std_by_var(norm_data_x, col_names_x, col_names, delevelwise=True)
     print("qtend_check: ", norm_data_y.mean(), norm_data_y.std())
     
 def test_single_column_multistep1():
@@ -225,6 +275,6 @@ def test_image_multistep1():
 if __name__ == "__main__":
     test_single_column_multistep0()
     # test_image_multistep0()
-    test_single_column_multistep1()
+    # test_single_column_multistep1()
     # test_image_multistep1()
 
