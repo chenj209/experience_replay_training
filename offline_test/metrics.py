4e3b9cfcf0802ab8986249da33ec11acef6147b3
chenj209 <kinderjianda.chen@gmail.com>
2024-04-12 09:51:42 -0400

    fix

--- offline_test/metrics.py
+++ offline_test/metrics.py
@@ -215,6 +215,8 @@ def report_qtend_vert_tail(y_gt, qtend, tail, top=True):
         else:
             qt_mask = (y_gt[:,i] <= y_gt_qt[0]).astype(bool)
         print(qt_mask)
+        print(qtend[:,i].shape)
+        print(qt_mask.shape)
         qtend_qt_data = qtend[:,i][qt_mask]
         y_gt_qt_data = y_gt[:,i][qt_mask]
         results.append(Regression_Metrics(y_gt_qt_data, qtend_qt_data))
