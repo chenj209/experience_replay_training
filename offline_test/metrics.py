bb0048a029f74417661c80178be01d3c47853dc0
chenj209 <kinderjianda.chen@gmail.com>
2024-04-12 09:06:10 -0400

    fix again

--- offline_test/metrics.py
+++ offline_test/metrics.py
@@ -212,8 +212,8 @@ def report_qtend_vert_quantile(y_gt, qtend, quantile):
         # qtend_qt = np.quantile(qtend[:,i], [tail, 1-tail])
         y_gt_qt = np.quantile(y_gt[:,i], [tail, 1-tail])
         qt_mask = (y_gt[:,i] >= y_gt_qt[0]) & (y_gt[:,i] <= y_gt_qt[1])
-        qtend_qt_data = qtend[qt_mask]
-        y_gt_qt_data = y_gt[qt_mask]
+        qtend_qt_data = qtend[:,i][qt_mask]
+        y_gt_qt_data = y_gt[:,i][qt_mask]
         results.append(Regression_Metrics(y_gt_qt_data, qtend_qt_data))
     merged_results = {}
     for key in results[0].keys():
