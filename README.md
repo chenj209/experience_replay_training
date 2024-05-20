d8eeb8f10572a8d3536224bbeefb99b999b972ca
chenj209 <kinderjianda.chen@gmail.com>
2024-05-20 19:27:29 +0800

    fix vae training scripts

--- README.md
+++ README.md
@@ -91,18 +91,54 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
-  --sample_rate SAMPLE_RATE
+  --sample_rate SAMPLE_RATE sample rate for offline test to be performed
                         sample frequency to use
-  --thick
+  --thick               whether to test with thickness computation
   --region_mask REGION_MASK
-  --start_ts START_TS
-  --multistep MULTISTEP
-  --ex_input [EX_INPUT [EX_INPUT ...]]
-  --ex_input_prev [EX_INPUT_PREV [EX_INPUT_PREV ...]]
-  --data_means DATA_MEANS
-  --data_stds DATA_STDS
+  --start_ts START_TS   indicate the start timestep to perform the offline test, default to 35040, which uses all available timesteps in testing data (starts from 35040)
+  --multistep MULTISTEP number of multistep to be used as input (this should match the checkpoint being loaded)
+  --ex_input [EX_INPUT [EX_INPUT ...]] should match checkpoint config
+  --ex_input_prev [EX_INPUT_PREV [EX_INPUT_PREV ...]] should match checkpoint config
+  --data_means DATA_MEANS should match checkpoint config
+  --data_stds DATA_STDS   should match checkpoint config
+
+outputs:
+  out_json: file that stores overall metrics
+  ex_qtend_{out_json}_vert.npz: file that stores vertical metrics (along pressure levels)
+  ex_qtend_{out_json}_spatial.npz: file that stores spatial metrics (metrics for each grid point 96x144)
+```
+
+* Examples
+Offline test baseline model performance in twp region
+```
+python offline_test_newformat.py /share3/chenj209/ckpts_time/baseline_model_sampled12_multistep0_lr0.001_region_twp_region/checkpoint_epoch20.pth.tar baseline_twp_ts0.json --region_mask ../consts/twp_region.npy --data_means ../consts/twp_region_means.npz --data_stds ../consts/twp_region_stds.npz --multistep 0 
 ```
 ### VAE training 
+* Usage
+```
+usage: scripts_train_ae_combined.py [-h] [--multistep MULTISTEP] [--lr LR] [--region_mask REGION_MASK] [--data_means DATA_MEANS] [--data_stds DATA_STDS]
+                                    [--ex_input_prev [EX_INPUT_PREV]] [--ex_input [EX_INPUT]] [--rec_weight REC_WEIGHT] [--pred_weight PRED_WEIGHT]
+                                    [--ae_config AE_CONFIG] [--resume [RESUME]]
 
+optional arguments:
+  -h, --help            show this help message and exit
+  --multistep MULTISTEP
+                        number of previous timesteps to be used as inputs
+  --lr LR               learning rate (default to 0.001)
+  --region_mask REGION_MASK
+                        default to all 96x144 grid, otherwirse path to a region mask (in ../consts folder)
+  --data_means DATA_MEANS
+                        path to npz files that stores means for all variables (in ../consts folder), default to ../consts/all_means.npz
+  --data_stds DATA_STDS
+                        path to npz files that stores stds for all variables (in ../consts folder), default to ../consts/all_stds.npz
+  --ex_input_prev [EX_INPUT_PREV]
+                        input variables to be added for previous timesteps (in addition to Q,T,dTls,dqls,solin,ps,qtend,stend,SOLL,SOLLD,SOLS,SOLSD,FSDS
+  --ex_input [EX_INPUT]
+                        input variables to be added for previous timesteps (in addition to Q,T,dTls,dqls,solin,ps
+  --rec_weight REC_WEIGHT
+  --pred_weight PRED_WEIGHT
+  --ae_config AE_CONFIG
+  --resume [RESUME]     path to checkpoint to resume training
+  ```
 
 
--- training_scripts/scripts_train_ae_combined.py
+++ training_scripts/scripts_train_ae_combined.py
@@ -1,21 +1,32 @@
 import os
 import argparse
-from consts import *
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from configs import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--multistep", type=int, help="multistep", default=1)
+    parser.add_argument("--multistep", type=int, help="number of previous timesteps to be used as inputs", default=1)
     #parser.add_argument("--gpu", type=int, help="gpu index", default=0)
-    parser.add_argument("--lr", type=int, help="learning rate", default=0.001)
-    parser.add_argument("--ex_input", type=str, nargs="?")
-    parser.add_argument("--ex_input_prev", type=str, nargs="?")
-    parser.add_argument("--region_mask", type=str, default="all")
-    parser.add_argument("--data_means", type=str)
-    parser.add_argument("--data_stds", type=str)
+    parser.add_argument("--lr", type=float, help="learning rate (default to 0.001)", default=0.001)
+    parser.add_argument("--region_mask", type=str, default="all", 
+        help="default to all 96x144 grid, otherwirse path to a region mask (in ../consts folder)")
+    parser.add_argument("--data_means", type=str, default="../consts/all_means.npz",
+        help="path to npz files that stores means for all variables (in ../consts folder),\
+                default to ../consts/all_means.npz")
+    parser.add_argument("--data_stds", type=str, default="../consts/all_stds.npz",
+        help="path to npz files that stores stds for all variables (in ../consts folder),\
+                default to ../consts/all_stds.npz")
+    parser.add_argument("--ex_input_prev", type=str, nargs="?",
+        help="input variables to be added for previous timesteps \
+                (in addition to Q,T,dTls,dqls,solin,ps,qtend,stend,SOLL,SOLLD,SOLS,SOLSD,FSDS")
+    parser.add_argument("--ex_input", type=str, nargs="?",
+        help="input variables to be added for previous timesteps \
+                (in addition to Q,T,dTls,dqls,solin,ps")
     parser.add_argument("--rec_weight", type=float, default=0.4)
     parser.add_argument("--pred_weight", type=float, default=0.6)
     parser.add_argument("--ae_config", type=str, default=str)
-    parser.add_argument("--resume", type=str, nargs="?")
+    parser.add_argument("--resume", type=str, nargs="?", help="path to checkpoint to resume training")
     args = parser.parse_args()
 
     if args.ex_input is not None:
@@ -28,14 +39,14 @@ if __name__ == "__main__":
     else:
         ex_input_prev = []
 
-    DATA_DIR = "/home/users/data/nncam_data/image_set/"
-    if not os.path.exists(DATA_DIR):
-        DATA_DIR = "/data/nncam_data/image_set/"
-    if not os.path.exists(DATA_DIR):
-        #DATA_DIR = "/global/cfs/cdirs/m4359/zhangtao/nncam/image_set/"
-        DATA_DIR = "/pscratch/sd/c/chenjd21/spcam_new_data_32/"
+    # DATA_DIR = "/home/users/data/nncam_data/image_set/"
+    # if not os.path.exists(DATA_DIR):
+    #     DATA_DIR = "/data/nncam_data/image_set/"
+    # if not os.path.exists(DATA_DIR):
+    #     #DATA_DIR = "/global/cfs/cdirs/m4359/zhangtao/nncam/image_set/"
+    #     DATA_DIR = "/pscratch/sd/c/chenjd21/spcam_new_data_32/"
 
-    name = f"ae_model029_sampled12_0421_multistep{args.multistep}_weight{args.pred_weight}_{args.rec_weight}_{args.ae_config.rstrip('.json')}_EX_{'+'.join(ex_input)}_EXPREV_{'+'.join(ex_input_prev)}"
+    name = f"{VAE_CKPT_PREFIX}_sampled{SAMPLE_RATE}_multistep{args.multistep}_weight{args.pred_weight}_{args.rec_weight}_{args.ae_config.rstrip('.json')}_EX_{'+'.join(ex_input)}_EXPREV_{'+'.join(ex_input_prev)}"
 
     for epoch in ['200']:
         for num_blocks in ['7']:
@@ -57,7 +68,7 @@ if __name__ == "__main__":
                         commands = f"python train_ae_model_ex_combined.py --data_dir {DATA_DIR}" + " --output_type 0-29 --noise_std {} " \
                                 '--network {} --node_size {} --num_blocks {} --activation {} --dropout {} ' \
                                 '--train_batch {} --lr_strategy {} --lr {} --epoch {} --wd {} ' \
-                                '--checkpoint /pscratch/sd/c/chenjd21/ckpts_time/{} --multistep {} --sample_rate 12 --workers 16 --region_mask {} --data_means {} --data_stds {} ' \
+                                f"--checkpoint {CKPT_DIR}"+'/{} --multistep {} '+f"--sample_rate {SAMPLE_RATE} --workers 16"+' --region_mask {} --data_means {} --data_stds {} ' \
                                 '--rec_weight {} --pred_weight {} --ae_config {} ' \
                                 '--ex_input {} --ex_input_prev {}'.format(str(noise_std),
                                                                 network, node_size, num_blocks, activation,
