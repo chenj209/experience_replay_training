3ba77b0fd51667449028bdfd2d5571f9a2df3f1c
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-06-17 10:20:50 +0800

    CCS training

--- training_scripts/sample_legacy_training_command.sh
+++ training_scripts/sample_legacy_training_command.sh
@@ -14,7 +14,7 @@ python train_time_model_legacy_format.py  \
     --network resnet_output30
 sample_train_test
 
-#: <<'sampled12_qtend_legacy_training_with_new_vars'
+: <<'sampled12_qtend_legacy_training_with_new_vars'
 python train_time_model_legacy_format.py  \
     --data_dir /data/nncam_data/image_set/ \
     --ex_data_dir /data/chenj209/ex_dataset/ \
@@ -30,4 +30,22 @@ python train_time_model_legacy_format.py  \
     --network resnet_output30 \
     --train_batch 32 \
     --worker 4
-#sampled12_qtend_legacy_training_with_new_vars
+sampled12_qtend_legacy_training_with_new_vars
+
+#: <<'sampled12_qtend_legacy_training_with_new_vars_CAPE_CLOUD_SPPRECC'
+python train_time_model_legacy_format.py  \
+    --data_dir /data/nncam_data/image_set/ \
+    --ex_data_dir /data/chenj209/ex_dataset/ \
+    --checkpoint /share3/chenj209/ckpts_sampled12/conv_mem/baseline_model029_sampled12_multistep1_new_vars_CCS_region_all/ \
+    --data_means ../consts/all_means.npz \
+    --data_stds ../consts/all_stds.npz \
+    --multistep 1 \
+    --sample_rate 12 \
+    --input_vars QL T_nn_in dqvls_nn_in dTls_nn_in SOLIN SPPS UL VL LWUP CAPE \
+    --output_vars qtend_check \
+    --input_vars_prev FLNS FLNT CLOUD SPPRECC qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --lr_strategy coslr \
+    --network resnet_output30 \
+    --train_batch 32 \
+    --worker 4
+#sampled12_qtend_legacy_training_with_new_vars_CAPE_CLOUD_SPPRECC
