b567999b9b7ba7b7604477d307a8e8b55da5896b
chenj209 <kinderjianda.chen@gmail.com>
2024-06-17 17:28:20 +0800

    sample test scripts

---
+++ offline_test/sample_offline_test2step.sh
@@ -0,0 +1,9 @@
+python offline_test_newformat_2step.py \
+    --model029 ../training_scripts/ckpts_sampled12/conv_mem/baseline_model029_sampled12_multistep1_EX__EXPREV__region_all/checkpoint_epoch35.pth.tar \
+    --model3059 ../training_scripts/ckpts_sampled12/conv_mem/baseline_model3059_sampled12_multistep1_EX__EXPREV__region_all/checkpoint_epoch35.pth.tar \
+    --model6165 ../training_scripts/ckpts_sampled12/conv_mem/baseline_model6165_sampled12_multistep1_EX__EXPREV__region_all/checkpoint_epoch35.pth.tar \
+    --train_configs ../training_scripts/ckpts_sampled12/conv_mem/baseline_model029_sampled12_multistep1_EX__EXPREV__region_all/configs.txt \
+    --start_ts 0 \
+    test_out.json
+
+
