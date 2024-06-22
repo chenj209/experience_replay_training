c39c9568ee200a24d8c732de12e06a3a252424e7
chenj209 <kinderjianda.chen@gmail.com>
2024-06-22 13:53:51 +0800

    basic modding

--- training_scripts/replay_buffer.py
+++ training_scripts/replay_buffer.py
@@ -7,14 +7,14 @@ import utils.misc as utils
 import copy
 
 class replay_buff():
-    def __init__(self, data_loader, inp_shape=[69, 721, 1440], max_size=300, weighted=False) -> None:
+    def __init__(self, data_loader, inp_shape=[96*144, 65], max_size=300, weighted=False) -> None:
         self.ptr = 0
         self.size = 0
         self.input_shape = inp_shape
         self.max_size = max_size
         self.weighted = weighted
         self.data_loader = data_loader
-        self.sample_stride = data_loader.dataset.sample_stride
+        # self.sample_stride = data_loader.dataset.sample_stride
         self.dataset_max_idx = data_loader.dataset.get_maxidx()
         self.buffer = {}
         self.buffer['inp'] = np.zeros((max_size, *inp_shape), dtype=np.float32)
@@ -35,8 +35,9 @@ class replay_buff():
             if tar_idx[i][0] + self.sample_stride > self.dataset_max_idx or (self.weighted and counter[i][0] >= 44):
                 continue
             else:
-                self.buffer['inp'][self.ptr] = inp_data[i]
-                self.buffer['target_idx'][self.ptr][:] = (tar_idx[i] + self.sample_stride)[:]
+                self.buffer['inp'][self.ptr] = inp_data[i] # (96*144,65)
+                #self.buffer['target_idx'][self.ptr][:] = (tar_idx[i] + self.sample_stride)[:]
+                self.buffer['target_idx'][self.ptr][:] = tar_idx[i][:] # (1,)
                 if self.weighted:
                     self.buffer['counter'][self.ptr][:] = (counter[i] + 1)[:]
 
@@ -57,14 +58,22 @@ class replay_buff():
             tar_counter = copy.deepcopy(self.buffer['counter'][idx[0:batch_size]])
 
         target_data_list = []
+        input_data_list = []
 
         for i in range(batch_size):
-            target_data_list.append(self.data_loader.dataset.get_target(tar_idx[i][0]))
+            # target_data_list.append(self.data_loader.dataset.get_target(tar_idx[i][0]))
+            input_data, target_data = self.data_loader.dataset.get_target(tar_idx[i][0]) # shape (13824, 309), (13824, 65)
+            input_data[:,122:122+65] = inp[i]
+            input_data_list.append(input_data)
+            target_data_list.append(target_data)
+
+        input_data = np.stack(input_data_list, axis=0)
         target_data = np.stack(target_data_list, axis=0)
         del target_data_list
+        del input_data_list
         gc.collect()
 
         if self.weighted:
-            return inp, target_data, tar_idx, tar_counter
+            return input_data, target_data, tar_idx, tar_counter
         else:
-            return inp, target_data, tar_idx
+            return input_data, target_data, tar_idx
