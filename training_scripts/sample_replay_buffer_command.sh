6c881be8190bb43d87d1e2da83e1a5d81bb79477
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-06-24 22:25:46 +0800

    replay buffer training starts

--- training_scripts/sample_replay_buffer_command.sh
+++ training_scripts/sample_replay_buffer_command.sh
@@ -1,8 +1,8 @@
-: <<'sample_train_test'
+#: <<'sample_train_test'
 python train_time_model_replay_buffer.py  \
     --data_dir /data/nncam_data/image_set/ \
     --ex_data_dir /data/chenj209/ex_dataset/ \
-    --checkpoint ckpts_sampled12/conv_mem/test_all_models \
+    --checkpoint /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309/ \
     --data_means ../consts/all_means.npz \
     --data_stds ../consts/all_stds.npz \
     --multistep 1 \
@@ -10,10 +10,11 @@ python train_time_model_replay_buffer.py  \
     --input_vars QL T_nn_in dqvls_nn_in dTls_nn_in SOLIN SPPS \
     --output_vars qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
     --input_vars_prev qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --train_batch 8 \
     --lr_strategy coslr 
-sample_train_test
+#sample_train_test
 
-#: <<'sample_train_test'
+: <<'sample_train_test'
 python train_time_model_replay_buffer.py  \
     --data_dir ../dataloader/data \
     --ex_data_dir ../dataloader/ex_data \
@@ -26,5 +27,5 @@ python train_time_model_replay_buffer.py  \
     --output_vars qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
     --input_vars_prev qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
     --lr_strategy coslr 
-#sample_train_test
+sample_train_test
 
