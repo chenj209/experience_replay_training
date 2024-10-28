1f62d8116d3892c9f4575ad0247a801e1c27b5ad
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-10-28 21:48:17 +0800

    able to run both ep and baseline

--- offline_test/sample_precip_analysis_offline_test.sh
+++ offline_test/sample_precip_analysis_offline_test.sh
@@ -1,3 +1,4 @@
+:<<'ai002_test_Wang2022_precip_analysis'
 CUDA_VISIABLE_DEVICES=2 python offline_test_newformat.py \
     --output_type 0-29 \
     --norm_type minmax_legacy \
@@ -6,3 +7,16 @@ CUDA_VISIABLE_DEVICES=2 python offline_test_newformat.py \
     --multistep 0 \
     /cust_users/x-w19/nncam.ckpts/resmlp.2years.50epochs/0_29_nodesize512_num_blocks7_actrelu_bs1024_scheduler_coslr_lr0.001_ep50_noise0.0_wd0_dropout0/checkpoint.pth.tar \
     Wang2022_precip_analysis.json
+ai002_test_Wang2022_precip_analysis
+# :<<'ai002_test_EP1117_precip_analysis'
+CUDA_VISIABLE_DEVICES=2 python offline_test_newformat.py \
+    --output_type 0-29 \
+    --norm_type std \
+    --start_ts 35040 \
+    --sample_rate 12 \
+    --multistep 1 \
+    --data_means ../consts/all_means.npz \
+    --data_stds ../consts/all_stds.npz \
+    /zz/share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v2_seed1117_full/0_29_checkpoint_best_loss.pth.tar \
+    EP1117_precip_analysis.json
+# ai002_test_EP1117_precip_analysis
