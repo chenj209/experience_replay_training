443d210f93275bb827248f69cf982d5fe6e4bea5
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-10-29 14:30:30 +0800

    some refactor

--- offline_test/offline_test_newformat.py
+++ offline_test/offline_test_newformat.py
@@ -32,11 +32,7 @@ FlattenSpatialTransform, get_min_max_coords
 from normalization import get_inverse_newformat, inverse_data_var_names
 # from dataloader_time_embedded import TimeDatasetDisk as DatasetDisk
 from load_models import load_models
-from metrics import Regression_Metrics, Regression_Metrics_axis, reverse_operations, \
-    report_qtend, report_stend, report_rad_prog, report_rad_prog_individual, \
-    report_qtend_vert, report_stend_vert, report_qtend_spatial, report_stend_spatial, \
-    get_thickness_from_ps_1d, report_qtend_vert_quantile, report_qtend_vert_tail, \
-    report_metric, report_metric_vert, report_metric_spatial
+from metrics import get_thickness_from_ps_1d, report_metric, report_metric_vert, report_metric_spatial
 sys.path.append(os.path.join(sys.path[0], '..', 'utils'))
 from data_shape import to_inference_shape, inverse_to_inference_shape
 
@@ -55,12 +51,12 @@ def inverse_61_64(x):
     x[:,3] = (x[:,3]+1)/2*(1299)+0          # fsnt
     return x
 
-inverse = {}
-inverse['0_29']  = lambda x: (x+1)/2*(3.11e-6*2)-3.11e-6
-inverse['30_59'] = lambda x: (x+1)/2*(3.63*2)-3.63
-inverse['60']    = lambda x: (x+1)/2*(2.12e-6)
-inverse['61_64'] = lambda x: inverse_61_64(x)
-inverse['61_65'] = lambda x: inverse_61_65(x)
+legacy_inverse = {}
+legacy_inverse['0_29']  = lambda x: (x+1)/2*(3.11e-6*2)-3.11e-6
+legacy_inverse['30_59'] = lambda x: (x+1)/2*(3.63*2)-3.63
+legacy_inverse['60']    = lambda x: (x+1)/2*(2.12e-6)
+legacy_inverse['61_64'] = lambda x: inverse_61_64(x)
+legacy_inverse['61_65'] = lambda x: inverse_61_65(x)
 
 def inverse_output(args, y, data_means=None, data_stds=None):
     if args.norm_type == "std":
@@ -69,12 +65,13 @@ def inverse_output(args, y, data_means=None, data_stds=None):
         elif args.output_type == "30_59":
             y = y/data_stds["stend_check"]+data_means["stend_check"]
         elif args.output_type == "61_65":
+            # TODO: need to deal with 61_65 have different ordering
             raise NotImplementedError("61_65 is not implemented")
     elif args.norm_type == "minmax_legacy":
         if args.output_type == "0_29":
-            y = inverse['0_29'](y)
+            y = legacy_inverse['0_29'](y)
         elif args.output_type == "30_59":
-            y = inverse['30_59'](y)
+            y = legacy_inverse['30_59'](y)
         elif args.output_type == "61_65":
             raise NotImplementedError("61_65 is not implemented")
     return y
@@ -83,13 +80,7 @@ def inverse_output(args, y, data_means=None, data_stds=None):
 def offline_test(args, all_models, testloader, get_thickness, silent=False, save=False):
     problem_files = []
     test_time_begin = time.time()
-    epoch = 1
-    criterion = nn.MSELoss()
     y_pred = []
-    y_1 = []
-    y_2 = []
-    y_3 = []
-    y_4 = []
     y_gt = []
     if args.region_mask == "all":
         region_mask = np.ones((1,1,96,144))
@@ -107,48 +98,26 @@ def offline_test(args, all_models, testloader, get_thickness, silent=False, save
         # allow empty batch
         if batch[0].shape[0] == 0:
             continue
-        suffix = 'testing- epoch:{}| iters:{}/{} |'.format(epoch, iter+1, len(testloader))
         # file_names = batch[-1]
-        #model.eval()
+        all_models['model'].eval()
         with torch.no_grad():
             points_x, points_y, x_raw, y_raw = batch[:4]
             filename = batch[-1][0][0].split("/")[-1]
             points_x, points_y = points_x.reshape(-1, points_x.shape[-1]), \
                 points_y.reshape(-1, points_y.shape[-1])
-            if iter == 0:
-                print("saving: ", batch[-1])
-                np.savez(f"offline_test_{filename}", 
-                         points_x=points_x.cpu().numpy(), 
-                         points_y=points_y.cpu().numpy(),
-                         x_raw=x_raw.cpu().numpy(),
-                         y_raw=y_raw.cpu().numpy())
-            print(x_raw.shape)
             if get_thickness is not None:
                 x_raw = x_raw.permute((0,2,1))
                 x_raw = x_raw.reshape(-1, x_raw.shape[-1])
                 thickness = get_thickness(x_raw[:,-1].numpy())
-                #print("thickness:", thickness.shape)
-            #points_x, points_y = (points_x.float()).cuda(), (points_y.float()).cuda()
             points_x = (points_x.float()).to(device)
-            #y1 = inverse_output['qtend_check'](all_models['0_29'](points_x).detach()
-            #                                            .cpu().numpy())
-            y1 = all_models['model'](points_x).detach().cpu().numpy()
+            pred = all_models['model'](points_x).detach().cpu().numpy()
             points_y = points_y.cpu().numpy()
-            #points_y = points_y[0]
-            #y1 = y1[0]
-            #print("points_y:", points_y.shape, points_y.mean(), points_y.std())
-            #print("y1:", y1.shape, y1.mean(), y1.std())
-            if True:
-                if iter % 200 == 0:
-                    print(batch[-1])
-                    filename = batch[-1][0][-1].split("/")[-1][:-4]
-                    np.savez(f"offline_test_{filename}", gt=points_y, pred=y1)
             if get_thickness is not None:
-                y1 = y1* thickness * phys_consts.LATVAP
+                pred = pred*thickness*phys_consts.LATVAP
                 points_y = points_y*thickness*phys_consts.LATVAP
-            y_1.append(y1)
+            y_pred.append(pred)
             y_gt.append(points_y)
-            loss = np.mean((y1 - points_y)**2)
+            loss = np.mean((pred - points_y)**2)
             if loss < best_loss:
                 best_loss = loss
                 best_filenames = batch[-1]
@@ -156,52 +125,32 @@ def offline_test(args, all_models, testloader, get_thickness, silent=False, save
                 worst_loss = loss
                 worst_filenames = batch[-1]
         #print(f"testing {iter}/{len(testloader)}, r2: {r2_score(points_y.flatten(), y1.flatten())}", end='\r')
-        print(f"testing {iter}/{len(testloader)}, r2: {r2_score(points_y.flatten(), y1.flatten())}")
-        print(f"filename: {batch[-1]}, Q lev 0 mean std: {x_raw[0,0].mean()}, {x_raw[0,0].std()}")
+        print(f"testing {iter}/{len(testloader)}, r2: {r2_score(points_y.flatten(), y_pred.flatten())}")
+        # print(f"filename: {batch[-1]}, Q lev 0 mean std: {x_raw[0,0].mean()}, {x_raw[0,0].std()}")
     print(f"Best loss: {best_loss}, filenames: {best_filenames}")
     print(f"Worst loss: {worst_loss}, filenames: {worst_filenames}")
 
-    np.save(f"ex_{output_name}_{args.out_json.rstrip('.json')}_avg_pred.npy", np.mean(np.concatenate([y[None,] for y in y_1], axis=0), axis=0))
-    np.save(f"ex_{output_name}_{args.out_json.rstrip('.json')}_avg_gt.npy", np.mean(np.concatenate([y[None,] for y in y_gt], axis=0), axis=0))
+    np.save(
+        f"ex_{output_name}_{args.out_json.rstrip('.json')}_avg_pred.npy",
+        np.mean(np.concatenate([y[None,] for y in y_1], axis=0), axis=0)
+        )
+    np.save(
+        f"ex_{output_name}_{args.out_json.rstrip('.json')}_avg_gt.npy",
+        np.mean(np.concatenate([y[None,] for y in y_gt], axis=0), axis=0)
+        )
 
-    y_1 = np.concatenate(y_1, axis=0)
-    # y_2 = np.concatenate(y_2, axis=0)
-    #y_4 = np.concatenate(y_4, axis=0)
+    y_pred = np.concatenate(y_pred, axis=0)
     y_gt = np.concatenate(y_gt, axis=0)
 
-    test_time = time.time() - test_time_begin
-
-    # qtend_log = report_qtend(y_gt, y_1)
-    # qtend_log_lvl = report_qtend_vert(y_gt, y_1)
-    log = report_metric(y_gt, y_1)
-    log_lvl = report_metric_vert(y_gt, y_1)
-    if False:
-        for quantile in [0.5,0.7,0.9,1]:
-            print(f"Quantile {quantile} results:")
-            qtend_log_lvl = report_qtend_vert_quantile(y_gt, y_1, quantile)
-            print(qtend_log_lvl["r2"])
-        for tail in [0.1,0.2,0.3]:
-            print(f"tail {tail} results:")
-            qtend_log_lvl = report_qtend_vert_tail(y_gt, y_1, tail, top=True)
-            print("Top:", qtend_log_lvl["r2"])
-            qtend_log_lvl = report_qtend_vert_tail(y_gt, y_1, tail, top=False)
-            print("Bottom:", qtend_log_lvl["r2"])
+    log = report_metric(y_gt, y_pred)
+    log_lvl = report_metric_vert(y_gt, y_pred)
     if args.region_mask == "all":
-        #qtend_log_spatial = report_qtend_spatial(y_gt, y_1)
         log_spatial = report_metric_spatial(y_gt, y_1, (y_1.shape[-1], 96, 144))
-    # qtend_log_spatial = report_qtend_spatial(y_gt, y_1)
-    #print(json.dumps(qtend_log_lvl, indent=4))
     del y_1
 
     res = {
         "log": log,
-        # "stend_log": stend_log,
-        #"rad_log": rad_log,
-        #"rad_log_individual": rad_log_individual,
         "log_lvl": log_lvl,
-        # "qtend_log_spatial": qtend_log_spatial,
-        # "stend_log_lvl": stend_log_lvl,
-        # "stend_log_spatial": stend_log_spatial
     }
     if args.region_mask == "all":
         res["log_spatial"] = log_spatial
@@ -278,34 +227,16 @@ if __name__ == "__main__":
                     setattr(args, key, train_configs[key])
         print("After train_configs overwrite:", args)
 
-    #print(args.config)
-    # with open(args.config, "r") as f:
-        # config = json.load(f)
-    # model_ckpt_path = config["0-29"]["ckpt_path"]
     model_ckpt_path = args.resume
-    # all_models = load_models(
-    #     config["0-29"]["ckpt_path"],
-    #     # config["30-59"]["ckpt_path"],
-    #     None,
-    #     None,
-    #     #config["61-65"]["ckpt_path"]
-    #     None,
-    #     model_type="resmlp_122_30"
-    # )
     np.random.seed(0)
-    #data_dir = "/home/users/data/nncam_data/image_testset/"
-    #if not os.path.isdir(data_dir):
-    #    data_dir = "/data/nncam_data/image_testset/"
-    #if not os.path.isdir(data_dir):
-    #    #data_dir = "/global/cfs/cdirs/m4359/zhangtao/nncam/image_testset/"
-    #    data_dir = "/pscratch/sd/c/chenjd21/spcam_new_data_32/"
     data_dir = DATA_DIR
     print("Test set path: ", data_dir)
 
     cudnn.benchmark = True
     col_names = np.loadtxt(data_dir + "/col_names.txt", dtype=str)
     col_names_x = ["QL", "T_nn_in", "dqvls_nn_in", "dTls_nn_in", "SOLIN", "SPPS"]+args.ex_input
-    prev_ex_vars = ["qtend_check", "stend_check", "SOLL", "SOLLD", "SOLS", "SOLSD", "FSDS"]+args.ex_input_prev
+    prev_ex_vars = ["qtend_check", "stend_check", "SOLL", "SOLLD", "SOLS", "SOLSD", "FSDS"]\
+        +args.ex_input_prev
     # col_names_y = ["qtend_check"]
     if args.output_type == '0-29':
         #batch[1] = batch[1][:, :, :30]
@@ -316,17 +247,12 @@ if __name__ == "__main__":
         col_names_y = ["stend_check"]
         output_size = 30
     elif args.output_type == '61-65':
-        #batch[1] = batch[1][:, :, 61:66]
-        #col_names_y = ["SOLL","SOLLD","SOLS","SOLSD","FSDS"]
         col_names_y = ["SOLL","SOLS","SOLLD","SOLSD","FSDS"]
         output_size = 5
-#             if args.output_type == '61-65':
     else:
         col_names_y = [args.output_type]
         output_size = 1
     output_name = '_'.join(col_names_y)
-    #data_means = dict(np.load(data_dir + "/data_means.npz"))
-    #data_stds = dict(np.load(data_dir + "/data_stds.npz"))
 
     all_files = glob.glob(data_dir+'/*.npy')
     all_files.sort()
@@ -415,31 +341,9 @@ if __name__ == "__main__":
 
     with open(args.out_json, "w") as f:
         json.dump({
-            # "dq/dt": logs["qtend_log"],
             args.output_type: logs["log"],
-            #"dT/dt": logs["stend_log"],
-            #"radiation": rad_log,
-            #"dqdt_lvl": qtend_log_lvl,
-            #"dTdt_lvl": stend_log_lvl,
-            #**logs["rad_log_individual"]
             }, f, indent=4)
-    # with open("ex_"+args.out_json, "w") as f:
-    #     json.dump({
-    #         #"dq/dt": qtend_log,
-    #         #"dT/dt": stend_log,
-    #         "radiation": logs["rad_log"],
-    #         "dqdt_lvl": logs["qtend_log_lvl"],
-    #         "dTdt_lvl": logs["stend_log_lvl"],
-    #         #**rad_log_individual
-    #         }, f, indent=4)
 
     if args.region_mask == "all":
         np.savez(f"ex_{output_name}_{args.out_json.rstrip('.json')}_spatial.npz", **logs["log_spatial"])
-    #np.savez(f"ex_stend_{args.out_json.rstrip('.json')}_spatial.npz", **logs["stend_log_spatial"])
     np.savez(f"ex_{output_name}_{args.out_json.rstrip('.json')}_vert.npz", **logs["log_lvl"])
-    #np.savez(f"ex_stend_{args.out_json.rstrip('.json')}_vert.npz", **logs["stend_log_lvl"])
-
-
-
-
-
