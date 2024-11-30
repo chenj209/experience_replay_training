ccb7c921f80a9edee75caa7859e24e5d607f3e50
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-12-01 05:56:16 +0800

    new test_data locl

--- offline_test/sample_offline_test1step.sh
+++ offline_test/sample_offline_test1step.sh
@@ -8,7 +8,7 @@ python offline_test_newformat_2step.py \
     test_out.json
 local_test
 
-#:<<'ai002_test'
+:<<'ai002_test'
 python offline_test_newformat_1step.py \
     --model029 /share3/chenj209/ckpts_sampled12/conv_mem/baseline_model029_sampled12_multistep1_EX__EXPREV__region_all/checkpoint_epoch35.pth.tar \
     --model3059 /share3/chenj209/ckpts_sampled12/conv_mem/baseline_model3059_sampled12_multistep1_EX__EXPREV__region_all/checkpoint_epoch35.pth.tar \
@@ -18,4 +18,19 @@ python offline_test_newformat_1step.py \
     --sample_rate 144 \
     --multistep 1 \
     test_out.json
+ai002_test
+
+
+
+#:<<'ai002_test'
+python offline_test_newformat_1step.py \
+    --model029 /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v2_seed1117_full_noprevQT_mix0.5/0_29_checkpoint_best_loss.pth.tar \
+    --model3059 /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v2_seed1117_full_noprevQT_mix0.5/30_59_checkpoint_best_loss.pth.tar \
+    --model6165 /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v2_seed1117_full_noprevQT_mix0.5/61_65_checkpoint_best_loss.pth.tar \
+    --train_configs /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v2_seed1117_full_noprevQT_mix0.5/configs.txt \
+    --start_ts 35040 \
+    --sample_rate 144 \
+    --multistep 1 \
+    --no_prevQT \
+    mix0.5.json
 #ai002_test
