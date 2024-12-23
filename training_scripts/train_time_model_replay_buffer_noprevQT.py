bd974fa29d7446bf5d2d5170f9fc67106acd59b4
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2024-12-24 07:25:49 +0800

    slice frist

--- training_scripts/train_time_model_replay_buffer_noprevQT.py
+++ training_scripts/train_time_model_replay_buffer_noprevQT.py
@@ -29,7 +29,7 @@ from dataloader_replay_buffer import DatasetDisk, filter_collate
 from dataloader_stride import DatasetDisk as TestDatasetDisk
 from replay_buffer import ReplayBuffer
 from preprocess import FlattenSpatialTransformNext, StandardizeTransformNext, RegionMaskTransform
-from preprocess import FlattenSpatialTransform, StandardizeTransform, MinMaxTransformLegacy2step, MinMaxTransformLegacy2stepNoFSDS
+from preprocess import FlattenSpatialTransform, StandardizeTransform, MinMaxTransformLegacy2step, MinMaxTransformLegacy2stepNext
 from dataloader_utils import gen_multistep_col_indices, get_index_from_colnames, \
     gen_col_indices
 
@@ -172,7 +172,7 @@ def main(args):
                 ])
         else:
             traintransform = transforms.Compose([
-                MinMaxTransformLegacy2step(),
+                MinMaxTransformLegacy2stepNext(),
                 FlattenSpatialTransformNext()
                 ])
             testtransform = transforms.Compose([
@@ -348,9 +348,9 @@ def main(args):
             if replay_buffer.size >= buffer_sample_size:
                 sampled_replay = replay_buffer.sample(buffer_sample_size)
                 sr_input, sr_target = sampled_replay[:2]
-                sr_input = torch.from_numpy(sr_input)
+                sr_input = torch.from_numpy(sr_input[:,:,60])
                 sr_target = torch.from_numpy(sr_target)
-                batch_input = torch.cat([batch[0], sr_input], dim=0)
+                batch_input = torch.cat([batch[0][:,:,:60], sr_input], dim=0)
                 #batch_input = torch.from_numpy(np.concatenate([batch[0], sr_input], axis=0)) 
                 #batch_targets = {
                 #    "0_29": torch.from_numpy(np.concatenate([batch[1][:,:,:30], sr_target[:,:,:30]], axis=0)),
@@ -364,13 +364,13 @@ def main(args):
                     "61_65": batch_target[:,:,60:]
                 }
             else:
-                batch_input = batch[0]
+                batch_input = batch[0][:,:,:60]
                 batch_targets = {
                     "0_29": batch[1][:,:,:30],
                     "30_59": batch[1][:,:,30:60],
                     "61_65": batch[1][:,:,60:]
                 }
-            batch_input = batch_input[:,:,60:] # remove prevQT
+            #batch_input = batch_input[:,:,60:] # remove prevQT
             print(f"prep data: {time.time() - prep_data_start}")
             train_mses = {}
             model_preds = {}
