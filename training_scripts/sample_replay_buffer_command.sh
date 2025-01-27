022458e08ed1ed6418d4a4f216d88d7cc28c598e
chenj209 <chenjd21@mails.tsinghua.edu.cn>
2025-01-27 15:18:11 +0800

    done running std

--- training_scripts/sample_replay_buffer_command.sh
+++ training_scripts/sample_replay_buffer_command.sh
@@ -2,6 +2,48 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train_time_model_replay_buffer_noprevQT.py  \
     --data_dir /data/nncam_data/image_set/ \
     --ex_data_dir /data/chenj209/ex_dataset/ \
+    --checkpoint /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v3_seed1117_sample2_noprevQT_std_mix1.0_rbs144/ \
+    --norm_type std \
+    --data_means ../consts/all_means.npz \
+    --data_stds ../consts/all_stds.npz \
+    --multistep 1 \
+    --sample_stride 2 \
+    --input_vars QL T_nn_in dqvls_nn_in dTls_nn_in SOLIN SPPS \
+    --output_vars qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --input_vars_prev qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --train_batch 24 \
+    --epoch 50 \
+    --lr_strategy coslr \
+    --manualSeed 1117 \
+    --buffer_size 144 \
+    --workers 24 \
+    --mixing_ratio 1.0
+#sample_train_test
+: <<'sample_train_test'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_time_model_replay_buffer_noprevQT.py  \
+    --data_dir /data/nncam_data/image_set/ \
+    --ex_data_dir /data/chenj209/ex_dataset/ \
+    --checkpoint /share3/chenj209/ckpts_sampled12/conv_mem/replay_buffer309_v3_seed1117_sample2_noprevQT_std_mix1.0_rbs24/ \
+    --norm_type std \
+    --data_means ../consts/all_means.npz \
+    --data_stds ../consts/all_stds.npz \
+    --multistep 1 \
+    --sample_stride 2 \
+    --input_vars QL T_nn_in dqvls_nn_in dTls_nn_in SOLIN SPPS \
+    --output_vars qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --input_vars_prev qtend_check stend_check SOLL SOLS SOLSD SOLLD FSDS \
+    --train_batch 24 \
+    --epoch 50 \
+    --lr_strategy coslr \
+    --manualSeed 1117 \
+    --buffer_size 24 \
+    --workers 24 \
+    --mixing_ratio 1.0
+sample_train_test
+: <<'sample_train_test'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_time_model_replay_buffer_noprevQT.py  \
+    --data_dir /data/nncam_data/image_set/ \
+    --ex_data_dir /data/chenj209/ex_dataset/ \
     --checkpoint /share3/chenj209/ckpts_sampled12/conv_mem/noreplay_buffer309_v3_seed1117_sample2_noprevQT_std/ \
     --norm_type std \
     --data_means ../consts/all_means.npz \
@@ -18,7 +60,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python train_time_model_replay_buffer_noprevQT.py  
     --buffer_size 0 \
     --workers 24 \
     --mixing_ratio 1.0
-#sample_train_test
+sample_train_test
 : <<'sample_train_test'
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train_time_model_replay_buffer_noprevQT.py  \
     --data_dir /data/nncam_data/image_set/ \
